252db920420562af6ee1493d9c812b963f30e263
苏向夜 <fu050409@163.com>
2023-11-20 11:06:16 +0800

    :bug: 修复路由未传入方法的错误

--- todos/urls.py
+++ todos/urls.py
@@ -1,16 +1,24 @@
 from django.urls import path
-from .views import login, new_task, new_project, new_todo, take_project, take_task
+from .views import (
+    login,
+    new_task,
+    new_project,
+    new_todo,
+    take_project,
+    take_task,
+    get_project,
+    get_task,
+    get_todo,
+)
 
 urlpatterns = [
     path("login/", login),
-
     path("project/new/", new_project),
     path("task/new/", new_task),
     path("todo/new/", new_todo),
     path("project/take/", take_project),
     path("task/new/", take_task),
-
-    path("/project/get/"),
-    path("/task/get/"),
-    path("/todo/get/"),
+    path("/project/get/", get_project),
+    path("/task/get/", get_task),
+    path("/todo/get/", get_todo),
 ]
